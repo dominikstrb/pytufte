8da5d311b9b529ec061d527a8d6085059408b513
Dominik Straub <dstraub93@gmail.com>
2020-04-24 17:11:57 +0200

    Add mplstyle and figsize thing

--- pytufte.py
+++ pytufte.py
@@ -1,11 +1,22 @@
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 
-mpl.rcParams["axes.spines.top"] = False
-mpl.rcParams["axes.spines.right"] = False
-mpl.rcParams["legend.frameon"] = False
+plt.style.use("tufte.mplstyle")
+
+GOLDEN_RATIO = 1.61803
+DEFAULT_FIGSIZE = 4.7747
+
+def get_figsize(width=1/2, ratio=GOLDEN_RATIO, base_figsize=DEFAULT_FIGSIZE):
+    """ Get the figure size for a Latex document in inches
+    Args:
+        width (float): width in terms of textwidth in a Latex doc that the figure should occupy
+        base_figsize (float): width of the page
+        ratio (float): height = width / ratio
+    Returns:
+        (width, height) tuple of floats
+    """
+    return base_figsize * width, base_figsize * width / ratio
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
     import numpy as np
 
     x = np.linspace(0, 2 * np.pi)
